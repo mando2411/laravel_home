7eba566b7872e46d90a958b6b891aa367b2e517a
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-14 12:27:23 +0200

    Auto update 2026-02-14 12:27

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,8 @@
+<h1>Latest Products</h1>
+
 @foreach($products as $product)
     <div>
         <h3>{{ $product->post_title }}</h3>
-        <a href="/product/{{ $product->post_name }}">View</a>
+        <a href="/product/{{ $product->post_name }}">View Product</a>
     </div>
 @endforeach
