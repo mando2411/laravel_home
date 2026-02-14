fa915fa1cd292af1365f11b057a16701007e2043
mando2411 <10312023101163@stud.cu.edu.eg>
2026-02-14 12:30:41 +0200

    Auto update 2026-02-14 12:30

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Cache;
 
 Route::get('/', function () {
 
-    $products = DB::table('wp_posts as p')
-        ->leftJoin('wp_postmeta as price', function ($join) {
-            $join->on('p.ID', '=', 'price.post_id')
-                 ->where('price.meta_key', '_price');
-        })
-        ->leftJoin('wp_postmeta as regular', function ($join) {
-            $join->on('p.ID', '=', 'regular.post_id')
-                 ->where('regular.meta_key', '_regular_price');
-        })
-        ->leftJoin('wp_postmeta as thumb', function ($join) {
-            $join->on('p.ID', '=', 'thumb.post_id')
-                 ->where('thumb.meta_key', '_thumbnail_id');
-        })
-        ->leftJoin('wp_posts as img', 'thumb.meta_value', '=', 'img.ID')
-        ->where('p.post_type', 'product')
-        ->where('p.post_status', 'publish')
-        ->orderBy('p.post_date', 'desc')
-        ->select(
-            'p.ID',
-            'p.post_title',
-            'p.post_name',
-            'price.meta_value as price',
-            'regular.meta_value as regular_price',
-            'img.guid as image'
-        )
-        ->limit(8)
-        ->get();
+    $products = Cache::remember('home_products', 300, function () {
+
+        return DB::table('wp_posts as p')
+            ->leftJoin('wp_postmeta as price', function ($join) {
+                $join->on('p.ID', '=', 'price.post_id')
+                     ->where('price.meta_key', '_price');
+            })
+            ->leftJoin('wp_postmeta as regular', function ($join) {
+                $join->on('p.ID', '=', 'regular.post_id')
+                     ->where('regular.meta_key', '_regular_price');
+            })
+            ->leftJoin('wp_postmeta as thumb', function ($join) {
+                $join->on('p.ID', '=', 'thumb.post_id')
+                     ->where('thumb.meta_key', '_thumbnail_id');
+            })
+            ->leftJoin('wp_posts as img', 'thumb.meta_value', '=', 'img.ID')
+            ->where('p.post_type', 'product')
+            ->where('p.post_status', 'publish')
+            ->orderBy('p.post_date', 'desc')
+            ->select(
+                'p.ID',
+                'p.post_title',
+                'p.post_name',
+                'price.meta_value as price',
+                'regular.meta_value as regular_price',
+                'img.guid as image'
+            )
+            ->limit(8)
+            ->get();
+
+    });
 
     return view('home', compact('products'));
 });
